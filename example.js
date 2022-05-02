dfd1d9790cc80bb7516adb1ef6c6558b191d643a
Andrew Hodel <andrewhodel@gmail.com>
2022-05-02 05:29:55 +0000

    added update with upsert to example

--- example.js
+++ example.js
@@ -11,8 +11,8 @@ console.log('\nmydb.insert({second_simple_field: \'second_simple_value\'})');
 var doc = mydb.insert({second_simple_field: 'second_simple_value'});
 console.log(util.inspect(doc, true, 10, true));
 
-console.log('\nmydb.insert({second_simple_field: \'2nd second_simple_value\'})');
-var doc = mydb.insert({second_simple_field: 'a second_simple_value'});
+console.log('\nmydb.update({second_simple_field: \'2nd second_simple_value\'}, {$set: {second_simple_field: \'2nd second_simple_value\'}}, {upsert: true})');
+var doc = mydb.update({second_simple_field: '2nd second_simple_value'}, {$set: {second_simple_field: '2nd second_simple_value'}}, {upsert: true});
 console.log(util.inspect(doc, true, 10, true));
 
 console.log('\nmydb.find({})');
